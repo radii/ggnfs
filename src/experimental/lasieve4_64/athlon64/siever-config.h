78d3ed1b2ed13d8ee43663c2d85bbeb819c2ca5c
Batalov <Batalov@80ef69ca-9341-0410-9d60-8acc7774fa31>
2009-11-06 23:52:37 +0000

    better L1_BITS definition
    git-svn-id: https://ggnfs.svn.sourceforge.net/svnroot/ggnfs/trunk@381 80ef69ca-9341-0410-9d60-8acc7774fa31

--- src/experimental/lasieve4_64/athlon64/siever-config.h
+++ src/experimental/lasieve4_64/athlon64/siever-config.h
@@ -5,7 +5,17 @@
 #define __SIEVER_CONFIG_H__
 #include <stddef.h> 
 #include <limits.h> 
+
+#if defined(__amdfam10__) || defined(__k8__) || defined(__athlon__)
 #define L1_BITS 16
+#else
+#define L1_BITS 15
+/* L1_BITS 15 for Intel Core2 (32Kb L1 data cache), AMD K6 */
+/* L1_BITS 16 for Phenom/Opteron... AMD K7 and up  */
+/* L1_BITS 14 was here before. PentiumIII had 16Kb */
+/* L1_BITS 13 for Pentium4 ? (only 8Kb L1 Dcache)  */
+#endif
+
 #define ULONG_RI
 typedef unsigned u32_t;
 typedef int i32_t;
--- src/lasieve4/piii/siever-config.h
+++ src/lasieve4/piii/siever-config.h
@@ -20,11 +20,15 @@
 #include "ggnfs.h"
 #include "if.h"
 
+#if defined(__amdfam10__) || defined(__k8__) || defined(__athlon__)
+#define L1_BITS 16
+#else
 #define L1_BITS 15
 /* L1_BITS 15 for Intel Core2 (32Kb L1 data cache), AMD K6 */
 /* L1_BITS 16 for Phenom/Opteron... AMD K7 and up  */
 /* L1_BITS 14 was here before. PentiumIII had 16Kb */
 /* L1_BITS 13 for Pentium4 ? (only 8Kb L1 Dcache)  */
+#endif
 
 #define ULONG_RI
 #define HAVE_CMOV
