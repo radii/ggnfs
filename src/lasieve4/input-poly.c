0e9257985e96d6f76f301ffc43d1004ecb3eb0ca
Andy Isaacson <adi@hexapodia.org>
2011-04-23 12:33:03 -0700

    lasieve4: kill unused variable 'j'
    input-poly.c: In function ‘input_poly’:
    input-poly.c:151:12: warning: unused variable ‘j’
    
    j isn't used even under ifdef, so drop it.

--- src/lasieve4/input-poly.c
+++ src/lasieve4/input-poly.c
@@ -148,7 +148,7 @@ void input_poly(mpz_t N, mpz_t ** A, i32_t *adeg, mpz_t ** B,
                 i32_t *bdeg, mpz_t m, FILE *fp)
 /*******************************************************/
 { char  token[256], value[512], thisLine[1024];
-  int   i, j, cont=1, have_m=0;
+  int   i, cont=1, have_m=0;
   mpz_t tmpA, tmpB;
 
   *adeg = *bdeg = 0;                                                    
